a504fedf24cae8a21028a989dd56433c3f559e15
Owaiz Shaikh <your_github_email@example.com>
2026-05-29 19:43:14 +0530

    enable cors

--- backend/leadtime_backend/leadtime_backend/settings.py
+++ backend/leadtime_backend/leadtime_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://leadcalculator.vercel.app",
+]
