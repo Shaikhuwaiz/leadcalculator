2abbefd874775ae562efcfd91ae55917dcb94e44
Owaiz Shaikh <your_github_email@example.com>
2026-06-12 03:12:06 +0530

    Changed something

--- frontend/src/data/holidays.js
+++ frontend/src/data/holidays.js
@@ -1,54 +1,15 @@
 export const holidays = [
-  {
-    date: "05-25-2026",
-    name: "Memorial Day"
-  },
-  {
-    date: "07-04-2025",
-    name: "Independence Day"
-  },
-  {
-    date: "07-06-2025",
-    name: "Independence Day"
-  },
-  {
-    date: "09-01-2025",
-    name: "Labor Day"
-  },
-  {
-    date: "11-26-2025",
-    name: "Thanksgiving Day"
-  },
-  {
-    date: "11-27-2025",
-    name: "Thanksgiving Day"
-  },
-  {
-    date: "11-28-2025",
-    name: "Thanksgiving Day"
-  },
-  {
-    date: "12-24-2025",
-    name: "Christmas Day"
-  },
-  {
-    date: "12-25-2025",
-    name: "Christmas Day"
-  },
-  {
-    date: "12-26-2025",
-    name: "Christmas Day"
-  },
-  {
-    date: "12-31-2025",
-    name: "New Year's Eve"
-  },
-  {
-    date: "01-01-2026",
-    name: "New Year's Day"
-  },
-  {
-    date: "01-02-2026",
-    name: "New Year Holiday"
-  }
+  '2026-01-01',
+  '2026-01-02',
+  '2026-05-25',
+  '2026-07-03',
+  '2026-07-06',
+  '2026-09-07',
+  '2026-11-26',
+  '2026-11-27',
+  '2026-11-28',
+  '2026-12-24',
+  '2026-12-25',
+  '2026-12-26',
+  '2026-12-31'
 ];
