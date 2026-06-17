61779cffc33b30f1c4921cb976b8be74e32ef89a
Owaiz Shaikh <your_github_email@example.com>
2026-06-18 03:01:39 +0530

    added

--- frontend/src/utils/update.js
+++ frontend/src/utils/update.js
@@ -1,11 +1,18 @@
 const updates = [
   {
     date: "01-8-2026",
-    content: "Please note that Peter Millar orders with 60+ or 90+ ship dates do NOT qualify for Prebook Perks 🚫🎁.\nThis is different from other brands such as Golf / CO / D&L ⚠️.\n\n👉 Important: Do not select any Prebook Perks (for example, Free Freight) for Peter Millar orders based only on the ship date 📅.\n\n✅ Key Action:\nAlways double-check the order copy. ."
+    content: `Please note that Peter Millar orders with 60+ or 90+ ship dates do NOT qualify for Prebook Perks 🚫🎁.
+This is different from other brands such as Golf / CO / D&L ⚠️.
+
+👉 Important: Do not select any Prebook Perks (for example, Free Freight) for Peter Millar orders based only on the ship date 📅.
+
+✅ Key Action:
+Always double-check the order copy. Peter Millar orders should show NO Prebook Perks 🧾❌.
+If any perks appear, please remove them immediately.`
   },
   {
     date: "03-11-2026",
-    content: "3/11/2026 Peter Millar orders should show NO Prebook Perks 🧾❌.\nIf any perks appear, please remove them immediately"
+    content: "3/11/2026 . Peter Millar doesn't have minimum charges - Grace glaser"
   }
 ];
 
