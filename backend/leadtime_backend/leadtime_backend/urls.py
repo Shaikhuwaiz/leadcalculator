e116a6b4dd6703a36c384874f59dcdfe8fdb36d8
Owaiz Shaikh <Owaiz.Shaikh@pdc.com>
2026-09-01 13:19:29 +0530

    Fixed theinclude

--- backend/leadtime_backend/leadtime_backend/urls.py
+++ backend/leadtime_backend/leadtime_backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.http import JsonResponse
 from django.shortcuts import redirect
 from api.views import api
