565560d0043827641122b2476b083b8faddd76a7
Owaiz Shaikh <Owaiz.Shaikh@pdc.com>
2026-09-01 13:13:30 +0530

    Fixed URLS

--- backend/leadtime_backend/leadtime_backend/urls.py
+++ backend/leadtime_backend/leadtime_backend/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import urls
+
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
@@ -30,6 +30,6 @@ def root(request):
 urlpatterns = [
     path("", root),
     path("admin/", admin.site.urls),
-    path("api/", include(urls)),
+    path("api/", api.urls),
     path("test/", test),
 ]
